576a1e2d6465d8a0de45ad130a09bfb2fea7fa39
Lakshithabinav S S <126949013+Lakshithabinav@users.noreply.github.com>
2024-07-16 16:08:41 +0530

    Installed all dependecies ready for deveploment

---
+++ server/src/main/java/com/example/AbricateEngineering/AbricateEngineeringApplication.java
@@ -0,0 +1,13 @@
+package com.example.AbricateEngineering;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AbricateEngineeringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AbricateEngineeringApplication.class, args);
+	}
+
+}
---
+++ server/src/test/java/com/example/AbricateEngineering/AbricateEngineeringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.AbricateEngineering;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AbricateEngineeringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
